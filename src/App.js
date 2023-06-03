0f1e347095a389d9dd17fc06d5e869e8cb81856b
yıldız <mesel4461@gmail.com>
2023-06-03 22:00:15 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Box } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Box sx={{ backgroundColor: "#000" }}>
+        <Navbar />
+        <Routes></Routes>
+      </Box>
+    </Router>
+  );
 };
 
 export default App;

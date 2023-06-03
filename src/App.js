1114da6fa8e85033ee8d2bf524cc65c1870b3014
yıldız <mesel4461@gmail.com>
2023-06-03 22:20:25 +0300

    as

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Feed />} />
+          {/* <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} /> */}
         </Routes>
       </Box>
     </Router>

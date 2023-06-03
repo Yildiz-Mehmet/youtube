87a85771c64e7cbb16428d1f44d154c158ba5b8d
yıldız <mesel4461@gmail.com>
2023-06-03 22:15:13 +0300

    as

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Box } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components";
+import Feed from "./components/Feed";
 
 const App = () => {
   return (
     <Router>
       <Box sx={{ backgroundColor: "#000" }}>
         <Navbar />
-        <Routes></Routes>
+        <Routes>
+          <Route exact path="/" element={<Feed />} />
+        </Routes>
       </Box>
     </Router>
   );
